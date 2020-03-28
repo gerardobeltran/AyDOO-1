d202d7e97306b686f55101c2e4a0b9d9965b2629
Gerardo Beltrán <contacto@gerardobeltran.mx>
2020-03-27 23:25:34 -0700

    Update funciones.c

--- Arreglos/funciones.c
+++ Arreglos/funciones.c
@@ -25,6 +25,54 @@ int busquedaSecuencialOrdenada(int datos[], int tam, int buscado){
   }
 return enc;
 }
+// Busqueda Binaria
+int busquedaBinaria(int datos[], int tam, int buscar){
+     int bajo;           //< Posición más baja para buscar
+     int alto;           //< Posición más alta para buscar
+     int mitad;          //< Mitad del vector
+     int encontrado=0;   //< Se pone a uno si se encuentra el elemento
+
+     bajo=0;
+     alto=tam-1;
+     while((bajo<=alto)&&(!encontrado)){
+         mitad=(alto+bajo)/2;
+         if(buscar==datos[mitad])
+             encontrado=1;
+          else if(buscar<datos[mitad])
+                 alto=mitad-1;
+             else
+                 bajo=mitad+1;
+     }
+     if(encontrado)
+         return mitad;
+     else
+         return -1;
+ }
+
+// Ordenacion Rapida - Quick Sort
+void ordenacionRapida(int *datos, int tam) {
+  if (tam < 2) return;
+ 
+  int pivot = datos[tam / 2];
+ 
+  int i, j;
+  for (i = 0, j = tam - 1; ; i++, j--) {
+    while (datos[i] < pivot) i++;
+    while (datos[j] > pivot) j--;
+ 
+    if (i >= j) break;
+ 
+    int temp = datos[i];
+    datos[i]     = datos[j];
+    datos[j]     = temp;
+  }
+ 
+  ordenacionRapida(datos, i);
+  ordenacionRapida(datos + i, tam - i);
+}
+ 
+
+
 
 
 
