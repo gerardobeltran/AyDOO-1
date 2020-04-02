0790127ad50b75b3a550ff7d6a270c015e1d1599
gerardobeltran <contacto@gerardobeltran.mx>
2020-04-02 10:36:27 -0700

    AyDOO

---
+++ Matrices/Mayor de una matriz/prog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+  int M, N, t, i, may, valor;
+
+  scanf("%d %d", &M, &N );
+
+  t = M * N;
+
+  scanf ("%d", &may);
+
+  for (i=1; i < t; i++)
+  {
+    scanf("%d", &valor);
+    if (valor > may)
+      may = valor;
+  }
+
+  printf ("%d", may);
+
+  return 0;
+}
