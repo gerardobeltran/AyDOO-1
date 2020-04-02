81a02caf33a20658d4b73ad066b12508bf7cb0bb
gerardobeltran <contacto@gerardobeltran.mx>
2020-04-02 12:09:12 -0700

    Empleados ABC

--- Matrices/Matriz Diagonal/prog.c
+++ Matrices/Matriz Diagonal/prog.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main() {
-  int j, i, N, M, enc;
+  int j, i, N, M, enc, valor;
   scanf ("%d", &N);
   int mat[N][N];
   for (i=0; i<N; i++)
   {
     for (j=0; j<N; j++)
     {
-        scanf("%d" mat[i][j]);
+        scanf("%d", &mat[i][j]);
     }
   }
 valor = mat[0][0];
-enc = 1
+enc = 1;
 for (i=1; i<N && enc == 1; i++)
 {
       if (valor!= mat[i][i])
         enc = 0;
   }
-}
+
 
 if (enc == 1 )
   printf ("SI");
